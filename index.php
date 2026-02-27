cd6c3f1c9f6e98f3603d4583498c204f32390aea
DmitriiSochi <bubenindk@gmail.com>
2026-02-27 13:45:43 +0300

    Создал базовую структура сайта на php Она будет состоять из трех частей header main footer страница будет собиратся взависимости от того что именно захочет увидеть гость. также создана страница about

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+    $pageTitle = "Главная страница"; // Переменная для заголовка в header.php
+    require 'includes/header.php'; 
+?>
+
+<main>
+    <h1>Добро пожаловать на наш сайт!</h1>
+    <p>Это динамическая главная страница на PHP.</p>
+</main>
+
+<?php require 'includes/footer.php'; ?>
